6d0bf785fdff403a05074860d2e10704853a739a
Marcoshsc <marcoshscunha@hotmail.com>
2020-10-08 12:54:43 -0300

    integrar temporizador no algoritmo de vizinho mais próximo

--- algorithms/nearestNeighbour/classes.py
+++ algorithms/nearestNeighbour/classes.py
@@ -2,6 +2,7 @@ from typing import List
 from abs.genericAlgorithm import GenericAlgorithm, AlgorithmResult
 from algorithms.nearestNeighbour.implementation import vizinhoMaisProximo
 from common.util import getCost
+from common.timer import executeWithTimeOut
 import time
 
 
@@ -9,7 +10,7 @@ class NearestNeighbourTSP(GenericAlgorithm):
 
     def execute(self, graph) -> AlgorithmResult:
         initialTime = time.time()
-        path = vizinhoMaisProximo(graph)
+        path = executeWithTimeOut(vizinhoMaisProximo, graph)
         executionTime = time.time() - initialTime
         cost = getCost(graph, path)
         return AlgorithmResult(path, cost, executionTime)
