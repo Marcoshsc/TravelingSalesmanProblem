2fed09f2711db7deede2b076c8f80b2ca92171d8
Marcoshsc <marcoshscunha@hotmail.com>
2020-10-08 12:57:09 -0300

    integrar temporizador em algoritmo de força bruta

--- algorithms/bruteForce/classes.py
+++ algorithms/bruteForce/classes.py
@@ -2,13 +2,17 @@ from typing import List
 from abs.genericAlgorithm import GenericAlgorithm, AlgorithmResult
 from algorithms.bruteForce.implementation import bruteForceTSP
 from common.util import getCost
+from common.timer import executeWithTimeOut
 import time
 
 class BruteForceTSP(GenericAlgorithm):
 
     def execute(self, graph) -> AlgorithmResult:
-        initialTime = time.time()
-        path = bruteForceTSP(graph)
-        executionTime = time.time() - initialTime
-        cost = getCost(graph, path)
-        return AlgorithmResult(path, cost, executionTime)
+        try:
+            initialTime = time.time()
+            path = executeWithTimeOut(bruteForceTSP, graph)
+            executionTime = time.time() - initialTime
+            cost = getCost(graph, path)
+            return AlgorithmResult(path, cost, executionTime)
+        except:
+            return AlgorithmResult([], None, None)
