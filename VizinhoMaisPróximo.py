34b4bde4250b578a1879979c510f1c8c763e9e18
Marcoshsc <marcoshscunha@hotmail.com>
2020-10-08 09:56:18 -0300

    adicionar tipos a VizinhoMaisProximo.py, mudar nome da função, atualizar representação de caminho, adicionar retorno

--- VizinhoMaisPróximo.py
+++ VizinhoMaisPróximo.py
@@ -1,37 +1,32 @@
-def vizinho_mais_proximo(G,s):
+from typing import List
+
+
+def vizinhoMaisProximo(G: List[List[float]], s: int) -> List[int]:
     u = s
-    C = []
+    C: List[int] = [s]
     tam = len(G)
-    Q = []
+    Q: List[int] = []
     v = None
 
     for i in range(tam):
         Q.append(i)
     Q.remove(u)
-    print(Q)
 
     while (Q!=0):
-        menor = 99999999
+        menor = float('inf')
         continua = False
         for i in range(tam):
-            if (G[u][i] != 0 and G[u][i] < menor and Q.count(i) != 0):
+            if G[u][i] != 0 and G[u][i] < menor and Q.count(i) != 0:
                 menor = G[u][i]
                 v = i
-                print(v)
                 continua = True
         if continua:
-            C.append((u,v))
+            C.append(v)
             Q.remove(v)
             u = v
         else:
             break
 
-    C.append((u,s))
-    print(C)
-
-grafo = [[0,1,2,5],[1,0,3,0],[2,3,0,4],[5,0,4,0]]
-vizinho_mais_proximo(grafo,0)
-
-
-
+    C.append(s)
+    return C
         
