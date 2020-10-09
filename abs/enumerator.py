67ca3049be13f2f15be236075c2d370586c1769c
Marcoshsc <marcoshscunha@hotmail.com>
2020-10-09 14:38:56 -0300

    adicionar metodo para retornar valor enumerado a partir de um valor string

--- abs/enumerator.py
+++ abs/enumerator.py
@@ -5,3 +5,13 @@ class EnumAlgorithms(Enum):
 
     NEAREST_NEIGHBOUR = 0,
     BRUTE_FORCE = 1
+
+    @staticmethod
+    def FromString(value: str) -> 'EnumAlgorithms':
+        upperValue = value.upper()
+        if upperValue == 'NEARESTNEIGHBOUR':
+            return EnumAlgorithms.NEAREST_NEIGHBOUR
+        elif upperValue == 'BRUTEFORCE':
+            return EnumAlgorithms.BRUTE_FORCE
+        else:
+            raise Exception('Algoritmo Inválido.')
