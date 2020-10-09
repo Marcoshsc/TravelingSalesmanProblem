18baf7a5b8bf24dc31fb79f1639af833d822cbcc
Marcoshsc <marcoshscunha@hotmail.com>
2020-10-09 14:38:33 -0300

    atualizar CLI de comparação para funcionar no windows

--- comparison.py
+++ comparison.py
@@ -1,10 +1,11 @@
 from run.runner import compare
 import argparse
 
-parser = argparse.ArgumentParser()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
 
-parser.add_argument('-p', '--predef', action='store_true', help='Utilizar dados definidos no arquivo resources/dataToCompare.json')
+    parser.add_argument('-p', '--predef', action='store_true', help='Utilizar dados definidos no arquivo resources/dataToCompare.json')
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-compare(args.predef)
+    compare(args.predef)
